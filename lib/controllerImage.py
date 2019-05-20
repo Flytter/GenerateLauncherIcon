813c8796eb83ecf312fc28e43824767777cc7143
Daniel Bandeira <daniel@stayon.tech>
2019-05-20 16:52:28 -0300

    Added to transform in IOS

--- lib/controllerImage.py
+++ lib/controllerImage.py
@@ -23,8 +23,30 @@ class controllerImg:
             self.saveAndroid(144, 144, self.androidFolder, 'mipmap-xxhdpi')
             self.saveAndroid(192, 192, self.androidFolder, 'mipmap-xxxhdpi')
             self.saveAndroid(512, 512, self.androidFolder, 'GooglePlay')
+
             self.saveIOS(20, 20, self.iosFolder, 'Icons', 'Icon-App-20x20@1x.png')
+            self.saveIOS(40, 40, self.iosFolder, 'Icons', 'Icon-App-20x20@2x.png')
+            self.saveIOS(60, 60, self.iosFolder, 'Icons', 'Icon-App-20x20@3x.png')
+
+            self.saveIOS(29, 29, self.iosFolder, 'Icons', 'Icon-App-29x29@1x.png')
+            self.saveIOS(58, 58, self.iosFolder, 'Icons', 'Icon-App-29x29@2x.png')
+            self.saveIOS(87, 87, self.iosFolder, 'Icons', 'Icon-App-29x29@3x.png')
+
+            self.saveIOS(40, 40, self.iosFolder, 'Icons', 'Icon-App-40x40@1x.png')
+            self.saveIOS(80, 80, self.iosFolder, 'Icons', 'Icon-App-40x40@2x.png')
+            self.saveIOS(120, 120, self.iosFolder, 'Icons', 'Icon-App-40x40@3x.png')
+            
+            self.saveIOS(120, 120, self.iosFolder, 'Icons', 'Icon-App-60x60@2x.png')
+            self.saveIOS(180, 180, self.iosFolder, 'Icons', 'Icon-App-60x60@3x.png')
+
+            self.saveIOS(76, 76, self.iosFolder, 'Icons', 'Icon-App-76x76@1x.png')
+            self.saveIOS(152, 152, self.iosFolder, 'Icons', 'Icon-App-76x76@2x.png')
+
+            self.saveIOS(167, 167, self.iosFolder, 'Icons', 'Icon-App-83.5x83.5@2x.png')
+
+            self.saveIOS(1024, 1024, self.iosFolder, 'Icons', 'Icon-App-1024x1024@1x.png')
     
+            self.saveIOS(1024, 1024, self.iosFolder, 'AppStore', 'Icon-AppStore.png')
 
     def createFolder(self, path, countName):
         os.mkdir(path+'/output/'+countName)
