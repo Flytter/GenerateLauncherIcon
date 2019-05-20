ecc1885500af2c30e71a207854ea37c54dae73db
Daniel Bandeira <daniel@stayon.tech>
2019-05-20 16:34:03 -0300

    FInished android folder save

--- lib/controllerImage.py
+++ lib/controllerImage.py
@@ -1,6 +1,6 @@
 from lib.getItensFromEntrance import *
+import cv2
 import os
-
 
 class controllerImg:
     
@@ -9,10 +9,19 @@ class controllerImg:
         self.path = path
         self.nameFolder = arguments
         self.listItens = getItens().getFile(path)
+        self.img = cv2.imread(self.listItens[0])
+        self.androidFolder = self.path+'/output/'+self.nameFolder[1]+'/android'
 
     def resizeImage(self):
+
         for i in self.listItens:
             self.createFolder(self.path, self.nameFolder[1])
+            self.saveAndroid(72, 72, self.androidFolder, 'mipmap-hdpi')
+            self.saveAndroid(48, 48, self.androidFolder, 'mipmap-mdpi')
+            self.saveAndroid(96, 96, self.androidFolder, 'mipmap-xhdpi')
+            self.saveAndroid(144, 144, self.androidFolder, 'mipmap-xxhdpi')
+            self.saveAndroid(192, 192, self.androidFolder, 'mipmap-xxxhdpi')
+            self.saveAndroid(512, 512, self.androidFolder, 'GooglePlay')
     
 
     def createFolder(self, path, countName):
@@ -34,4 +43,8 @@ class controllerImg:
         os.mkdir(path+'/output/'+countName+'/IOS/Icons')
         os.mkdir(path+'/output/'+countName+'/IOS/AppStore')
 
+    def saveAndroid(self, width, height, local, filename):
+        image = cv2.resize(self.img,(width,height), interpolation = cv2.INTER_CUBIC)
+        print(local+'/'+filename+'/'+'ic_launcher.png')
+        cv2.imwrite(local+'/'+filename+'/'+'ic_launcher.png', image)
 
