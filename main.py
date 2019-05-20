5edc694f949384cdd5e79b3c58bf93f537bffead
Daniel Bandeira <daniel@stayon.tech>
2019-05-20 15:16:00 -0300

    Added arguments to main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from lib.getItensFromEntrance import getItens
 from lib.controllerImage import controllerImg
 import os
+import sys
 
 
+arguments = sys.argv
 pathDirect = os.getcwd()
 
-resizeImage = controllerImg(pathDirect)
+resizeImage = controllerImg(pathDirect, arguments)
 resizeImage.resizeImage()
